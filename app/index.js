4c29eff33536145e1d9fa4736cd2e0a85d267953
UsPortatil <jonathariflo@gmail.com>
2025-09-25 19:25:01 -0600

    feat: incio de login

--- app/index.js
+++ app/index.js
@@ -1,12 +1,29 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 export default function index() {
+  const [input, setInput] = useState("");
+
+  function imprrimirDatos() {
+    //enviar a servidor
+    console.log(input);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hola soy chis el gay</Text>
       <Link href='/about' style={{color: '#4024b3ff'}}>about</Link>
+
+      <TextInput onChange={(e) => setInput(e.nativeEvent.text)} >
+        <Text style={styles.text}></Text>
+      </TextInput>
+      
+       <TouchableOpacity style={styles.button} onPress={imprrimirDatos}>
+          <Text>Press Here</Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +32,16 @@ export default function index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  } 
 });
